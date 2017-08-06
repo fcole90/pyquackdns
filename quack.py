05957306323626b2949802a45052ce36b92a6983
Fabio Colella <fcole90@gmail.com>
2017-08-06 11:26:05 +0200

    Default configuration file is now in the same folder of quack.py

--- quack.py
+++ quack.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import json
+import os
 import requests
 import sys
 import time
 
 DUCK_DNS_URL = 'https://www.duckdns.org/update'
+DEFAULT_CONFIG_FILE_NAME = 'config_parameters.json'
+DEFAULT_CONFIG_PATH = os.path.dirname(os.path.realpath(__file__))
 
 #APIs at https://www.duckdns.org/spec.jsp
 '''
@@ -18,30 +21,7 @@ ip - OPTIONAL - if left blank we detect IPv4 addresses, if you want you can supp
 ipv6 - OPTIONAL - a valid IPv6 address, if you specify this then the autodetection for ip is not used
 verbose - OPTIONAL - if set to true, you get information back about how the request went
 clear - OPTIONAL - if set to true, the update will ignore all ip's and clear both your records
-'''
-
-def main():
-    
-    default_config_file_name = "config_parameters.json"
-    if len(sys.argv) < 2 or len(sys.argv) > 3 :
-        print_usage_error()
-    else:
-        # Check configuration parameter
-        if len(sys.argv) > 2:
-            config_file_name = sys.argv[2]
-        else:
-            config_file_name = default_config_file_name
-            print("Using default parameter file: " + default_config_file_name)
-        
-        # Options
-        if sys.argv[1] == 'configure':            
-            configure(config_file_name)
-        elif sys.argv[1] == 'start':
-            start(config_file_name)
-        else:
-            print_usage_error()
-       
-       
+'''    
 
 def print_usage_error():
     print("Quack: Keeps your DuckDNS IP information updated.\n"
@@ -53,7 +33,7 @@ def print_usage_error():
           "\t-h --help\t\t- print this help message")
 
 
-def configure(config_file_name):
+def configure(config_file_str):
     params = {}
     
     domain = input("Please, insert your domain name:\n>>> ")
@@ -65,15 +45,17 @@ def configure(config_file_name):
         'verbose': 'true'
     }
     
-    with open(config_file_name, 'w') as config_file:
+    with open(config_file_str, 'w') as config_file:
         json.dump(params, config_file)
     
     print("Saved, now you can start the service.")
 
 
-def start(config_file_name):    
-    with open(config_file_name, 'r') as config_file:
+def start(config_file_str):    
+    with open(config_file_str, 'r') as config_file:
         params = json.load(config_file)
+    
+    print("Updating \"" + params["domains"] + "\"..")
     
     while True:
         try:
@@ -81,12 +63,36 @@ def start(config_file_name):
                              params=params, 
                              verify=False)
             print(r.text)
-            time.sleep(10)
+            time.sleep(100)
         except:
             e = sys.exc_info()[0]
             print('Error: ' + str(e) + str())
-            time.sleep(10)
+            time.sleep(200)
 
+
+def main():
+    
+    default_config_file_str = os.path.join(DEFAULT_CONFIG_PATH,
+                                           DEFAULT_CONFIG_FILE_NAME)
+                                           
+    # Very trivial argument parsing
+    if len(sys.argv) < 2 or len(sys.argv) > 3 :
+        print_usage_error()
+    else:
+        # Check if a configuration file has been specified else use default
+        if len(sys.argv) > 2:
+            config_file_str = sys.argv[2]
+        else:
+            config_file_str = default_config_file_str
+            print("Using default parameter file: " + default_config_file_str)
+        
+        # Options
+        if sys.argv[1] == 'configure':            
+            configure(config_file_str)
+        elif sys.argv[1] == 'start':
+            start(config_file_str)
+        else:
+            print_usage_error()
 
 if __name__ == '__main__':
     main()
